6645b72b3f460dc0fbc9525dc57d166b7d53b93e
stevenhu <stevenhu_8882163.com>
2016-04-26 18:00:12 +0800

    初始化

---
+++ Application/User/Conf/config.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+/**
+ * UCenter客户端配置文件
+ * 注意：该配置文件请使用常量方式定义
+ */
+
+define('UC_APP_ID', 1); //应用ID
+define('UC_API_TYPE', 'Model'); //可选值 Model / Service
+define('UC_AUTH_KEY', 'wM>kP+KFoY0jp*_iA]}@Z|1(e7lcV,:T%&8ER!$H'); //加密KEY
+define('UC_DB_DSN', 'mysql://root:fo9B8OMD76Z2@127.0.0.1:3306/doubihai'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_TABLE_PREFIX', 'dbh_'); // 数据表前缀，使用Model方式调用API必须配置此项
