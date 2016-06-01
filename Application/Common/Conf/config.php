4d9ee62e89faaf38498796fce1ddedb8d793bab9
stevenhu <stevenhu_888@163.com>
2016-06-01 17:45:44 +0800

    修改数据库密码

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -43,7 +43,7 @@ return array(
     'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'doubihai', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    'DB_PWD'    => 'fo9B8OMD76Z2',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'dbh_', // 数据库表前缀
 
--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'wM>kP+KFoY0jp*_iA]}@Z|1(e7lcV,:T%&8ER!$H'); //加密KEY
-define('UC_DB_DSN', 'mysql://root:root@127.0.0.1:3306/doubihai'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:fo9B8OMD76Z2@127.0.0.1:3306/doubihai'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'dbh_'); // 数据表前缀，使用Model方式调用API必须配置此项
