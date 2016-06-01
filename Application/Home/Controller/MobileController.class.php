6f029bcc1621f2ecb8ea352f65f05d146eefe592
stevenhu <stevenhu_888@163.com>
2016-06-01 15:06:57 +0800

    手机分享页面修改

--- Application/Home/Controller/MobileController.class.php
+++ Application/Home/Controller/MobileController.class.php
@@ -11,6 +11,24 @@ class MobileController extends Controller {
     }
     
     public function share() {
+        $work_id = I('id', '', 'intval');
+        if(empty($work_id)) {
+            echo 'sorry,该作品不存在';die;
+        }
+        
+	    $Work = M('work');
+	    $detail = $Work->alias('w')
+	    ->field('w.id,m.uid,m.nickname,m.avatar,d.title,d.cover_id,w.material_id,w.cover_url,w.video_url,w.description,w.create_time,w.likes,w.views,w.comments')
+	    ->join('__DOCUMENT__ d on d.id = w.material_id', 'left')
+	    ->join('__DOCUMENT_MATERIAL__ dm on dm.id = d.id', 'left')
+	    ->join('__MEMBER__ m on m.uid = w.uid', 'left')
+	    ->where('w.id='.$work_id)
+	    ->find();
+	    
+	    $detail['create_time'] = date('Y-m-d', $detail['create_time']);
+        $detail['avatar'] = !empty($detail['avatar'])?$detail['avatar']:C('USER_INFO_DEFAULT.avatar');
+        
+        $this->assign('info', $detail);
         $this->display();
     }
 }
