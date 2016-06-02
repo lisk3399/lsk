f399a02b0c3f571ad720bbb8f9ce352608e89b15
stevenhu <stevenhu_888@163.com>
2016-06-02 20:00:42 +0800

    个人中心关注/取消bug修复

--- Application/Home/Controller/SnsController.class.php
+++ Application/Home/Controller/SnsController.class.php
@@ -206,14 +206,6 @@ class SnsController extends HomeController {
 	        
 	        //关注
 	        if($this->followOne($who_follow, $follow_who)) {
-	            //更新用户关注数
-	            $map['uid'] = $who_follow;
-	            M('member')->where($map)->setInc('follows');
-	            
-	            //更新被关注用户粉丝数
-	            $map['$uid'] = $follow_who;
-	            M('member')->where($map)->setInc('fans');
-	            
 	            $this->renderSuccess('关注成功');
 	        } else {
 	            $this->renderFailed('关注失败');
@@ -253,7 +245,7 @@ class SnsController extends HomeController {
 	            M('member')->where($map)->setDec('follows');
 	             
 	            //更新被关注用户粉丝数
-	            $map['$uid'] = $follow_who;
+	            $map['uid'] = $follow_who;
 	            M('member')->where($map)->setDec('fans');
 	            $this->renderSuccess('取消关注成功');
 	        } else {
