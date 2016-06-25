c8c64fff423726b2d20918567c085bc50cdbc085
stevenhu <stevenhu_888@163.com>
2016-06-25 11:20:09 +0800

    新增本地视频类型

--- Application/Home/Controller/WorkController.class.php
+++ Application/Home/Controller/WorkController.class.php
@@ -54,7 +54,7 @@ class WorkController extends HomeController {
             //设置素材封面图
             foreach ($list as &$row) {
                 $row['material_cover_url'] = !empty($row['cover_id'])?C('WEBSITE_URL').get_cover($row['cover_id'], 'path'):'';
-                if($row['type'] == 'DUBBING') {//配音秀
+                if($row['type'] == 'DUBBING' || $row['type']=="LOCAL") {//配音秀
                     $row['cover_url'] = "http://vod.doushow.com/400-400px.png";
                 }
                 if(!empty($row['topic_name'])) {
