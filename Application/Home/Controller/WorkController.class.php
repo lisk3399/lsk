a8b0c404d106141a06cafdff4178afa1dc43b04e
stevenhu <stevenhu_888@163.com>
2016-06-29 13:49:08 +0800

    最新作品列表原创作品标题默认显示原创

--- Application/Home/Controller/WorkController.class.php
+++ Application/Home/Controller/WorkController.class.php
@@ -54,11 +54,14 @@ class WorkController extends HomeController {
             //设置素材封面图
             foreach ($list as &$row) {
                 $row['material_cover_url'] = !empty($row['cover_id'])?C('WEBSITE_URL').get_cover($row['cover_id'], 'path'):'';
-                if($row['type'] == 'DUBBING' || $row['type']=="LOCAL") {//配音秀
+                if($row['type'] == 'DUBBING' || $row['type']=="LOCAL") {//配音秀及本地视频
                     $row['cover_url'] = "http://vod.doushow.com/400-400px.png";
-                }
+                } 
                 if(!empty($row['topic_name'])) {
                     $row['title'] = $row['topic_name'];
+                }
+                if($row['type']=="ORIGINAL") {
+                    $row['title'] = "原创";
                 }
                 unset($row['cover_id']);
             }
