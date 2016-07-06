fe802288dc940ddcf8df2067eb7b7134f2019ea5
stevenhu <stevenhu_888@163.com>
2016-07-06 14:22:15 +0800

    群组控制器初始化

---
+++ Application/Home/Controller/GroupController.class.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Class GroupController
+ * @name 群组管理
+ */
+
+namespace Home\Controller;
+
+class GroupController extends HomeController {
+	/**
+	 * 我创建的群组
+	 */
+	public function myGroup() {
+	    
+	}
+	
+	/**
+	 * 我加入的群组
+	 */
+	public function groupJoined(){
+	    
+	}
+	
+	/**
+	 * 创建群组
+	 */
+	public function createGroup() {
+	     
+	}
+	
+	/**
+	 * 搜索群组
+	 */
+	public function searchGroup() {
+	    
+	}
+	
+	/**
+	 * 某群组下的作品列表
+	 */
+	public function groupWorks($group_id) {
+	    
+	}
+	
+	/**
+	 * 某群组下的成员
+	 */
+	public function groupMembers($group_id) {
+	    
+	}
+	
+}
