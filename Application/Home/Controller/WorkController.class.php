cbb46d8a39d6596a4750c2bc8cd8eabb7d903d23
stevenhu <stevenhu_888@163.com>
2016-07-14 15:05:14 +0800

    修复本地上传视频播放背景图重复问题

--- Application/Home/Controller/WorkController.class.php
+++ Application/Home/Controller/WorkController.class.php
@@ -353,7 +353,7 @@ class WorkController extends HomeController {
         //设置素材封面图
         if(!empty($detail['cover_id'])) {
             $detail['material_cover_url'] = C('WEBSITE_URL').get_cover($detail['cover_id'], 'path');
-            if($detail['type'] == 'DUBBING') {//配音秀
+            if($detail['type'] == 'DUBBING' || $detail['type'] == 'LOCAL') {//配音秀或本地
                 $detail['cover_url'] = "http://vod.doushow.com/400-400px.png";
             }
         }
