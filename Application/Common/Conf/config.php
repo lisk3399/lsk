c38b44bf589e381b07af83a02468585d47d44372
stevenhu <stevenhu_888@163.com>
2016-08-03 16:36:56 +0800

    配置文件增加对用户参数的过滤

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -36,7 +36,7 @@ return array(
     'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
 
     /* 全局过滤配置 */
-    'DEFAULT_FILTER' => '', //全局过滤函数
+    'DEFAULT_FILTER' => 'strip_tags,htmlspecialchars', //全局过滤函数
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
