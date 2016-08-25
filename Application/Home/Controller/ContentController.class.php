eeee01343635ef1a6ffe5e8c0727ac903e3438b7
stevenhu <stevenhu_888@163.com>
2016-08-25 11:11:47 +0800

    评论点赞

--- Application/Home/Controller/ContentController.class.php
+++ Application/Home/Controller/ContentController.class.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Home\Controller;
-
 use User\Api\UserApi;
 
 class ContentController extends HomeController {
@@ -47,6 +46,7 @@ class ContentController extends HomeController {
 	                    'content_id'=>$content_id,
 	                    'type'=>$row['type'],
 	                    'value'=>$row['value'],
+	                    'cover_url'=>(!empty($row['cover_url'])?$row['cover_url']:''),
 	                    'create_time'=>$create_time
 	                );
 	            }
@@ -55,14 +55,17 @@ class ContentController extends HomeController {
 	            }
 	            $this->renderSuccess('添加成功');
 	        }
-	        
 	    }
 	}
-	
+
     private function is_valid_json($string) {
         json_decode($string);
         return (json_last_error() == JSON_ERROR_NONE);
     }
+    
+    public function viewContent() {
+        
+    }    
     
     public function contentList() {
         
@@ -79,5 +82,244 @@ class ContentController extends HomeController {
     public function addToDrafts() {
         
     }
+    
+    /**
+     * 某个作品评论列表
+     */
+    public function commentList() {
+        $work_id = I('id', '', 'intval');
+        if(empty($work_id)) {
+            $this->renderFailed('作品id为空');
+        }
+        if(!$this->checkWorkExists($work_id)) {
+            $this->renderFailed('作品不存在');
+        }
+    
+        $page = I('page', '1', 'intval');
+        $rows = I('rows', '20', 'intval');
+         
+        //限制单次最大读取数量
+        if($rows > C('API_MAX_ROWS')) {
+            $rows = C('API_MAX_ROWS');
+        }
+         
+        $Comment = M('comment');
+        $count = $Comment->alias('c')
+        ->field('m.uid')
+        ->join('__MEMBER__ m on m.uid = c.uid', 'left')
+        ->where(array('c.work_id'=>$work_id))->count();
+         
+        $list = $Comment->alias('c')
+        ->page($page, $rows)
+        ->field('m.uid,m.nickname,m.avatar,c.to_uid,c.content,c.create_time')
+        ->join('__MEMBER__ m on m.uid = c.uid', 'left')
+        ->where(array('c.work_id'=>$work_id))
+        ->order('c.id desc')
+        ->select();
+         
+        if(count($list) == 0) {
+            $this->renderFailed('没有更多了');
+        }
+        if($page>1 && count($list)==0) {
+            $this->renderFailed('没有更多了', -1);
+        }
+        //设置默认头像
+        $Api = new UserApi;
+        $list = $Api->setDefaultAvatar($list);
+        foreach ($list as &$row) {
+            $row['content'] = rawurldecode($row['content']);
+        }
+         
+        $extra['count'] = $count;
+        $this->renderSuccess('', $list, $extra);
+    }
+    
+    /**
+     * 发布评论
+     */
+    public function pubComment() {
+        if(IS_POST) {
+            $uid = is_login();
+            if(!$uid) {
+                $this->renderFailed('请先登录');
+            }
+            $work_id = I('id', '', 'intval');
+            if(empty($work_id)) {
+                $this->renderFailed('作品id为空');
+            }
+            if(!$this->checkWorkExists($work_id)) {
+                $this->renderFailed('作品不存在');
+            }
+    
+            $content = I('content', '', 'trim');
+            if(empty($content)) {
+                $this->renderFailed('请输入内容');
+            }
+    
+            $data['uid'] = $uid;
+            $data['work_id'] = $work_id;
+            $data['content'] = rawurlencode($content);
+            $data['create_time'] = NOW_TIME;
+             
+            //TODO:判断重复评论
+            // 	        if($this->checkComment($uid, $work_id)) {
+            //
+            // 	        }
+            $Comment = M('comment');
+            if($Comment->add($data)){
+                //更新评论数
+                $Content = M('Content');
+                $map = array('id' => $work_id);
+                $Content->where($map)->setInc('comments');
+                $this->renderSuccess('评论成功');
+            }
+            $this->renderFailed('评论失败');
+        }
+    }
+    
+    /**
+     * 作品点赞/喜欢
+     */
+    public function like() {
+        if(IS_POST) {
+            $uid = is_login();
+            if(!$uid) {
+                $this->renderFailed('请先登录');
+            }
+             
+            $work_id = I('id', '', 'intval');
+            if(empty($work_id)) {
+                $this->renderFailed('作品id为空');
+            }
+            if(!$this->checkWorkExists($work_id)) {
+                $this->renderFailed('作品不存在');
+            }
+            //判断是否已经点赞
+            if($this->checkLike($uid, $work_id)) {
+                $this->renderFailed('已经点过赞');
+            }
+             
+            //喜欢表增加对应关系
+            $data['uid'] = $uid;
+            $data['work_id'] = $work_id;
+            $data['create_time'] = NOW_TIME;
+            $Likes = M('likes');
+            if($Likes->data($data)->add()) {
+                //更新作品点赞数
+                $map['id'] = $work_id;
+                M('Content')->where($map)->setInc('likes');
+                //更新用户点赞数
+                $map['uid'] = $uid;
+                M('Member')->where($map)->setInc('likes');
+                $this->renderSuccess('点赞成功');
+                //TODO 发送消息
+            }
+            else {
+                $this->renderFailed('点赞失败');
+            }
+        }
+    }
+    
+    /**
+     * 取消点赞/喜欢
+     */
+    public function unlike() {
+        if(IS_POST) {
+            $uid = is_login();
+            if(!$uid) {
+                $this->renderFailed('请先登录');
+            }
+    
+            $work_id = I('id', '', 'intval');
+            if(empty($work_id)) {
+                $this->renderFailed('作品id为空');
+            }
+            if(!$this->checkWorkExists($work_id)) {
+                $this->renderFailed('作品不存在');
+            }
+    
+            //喜欢表去掉对应关系
+            $map['uid'] = $uid;
+            $map['work_id'] = $work_id;
+            $Likes = M('likes');
+            if($Likes->where($map)->delete()) {
+                //更新作品点赞数
+                $map['id'] = $work_id;
+                M('Content')->where($map)->setDec('likes');
+                //更新用户点赞数
+                $map['uid'] = $uid;
+                M('Member')->where($map)->setDec('likes');
+                
+                $this->renderSuccess('取消点赞');
+            }
+            else {
+                $this->renderFailed('取消点赞失败');
+            }
+        }
+    }
+    
+    /**
+     * 检查用户是否给某作品点赞
+     * @param int $uid
+     * @param int $work_id
+     */
+    private function checkLike($uid, $work_id) {
+        $Likes = M('likes');
+        $map['uid'] = $uid;
+        $map['work_id'] = $work_id;
+         
+        return $Likes->field('id')->where($map)->find();
+    }
+    
+    /**
+     * 更新作品喜欢数
+     * @param int $work_id
+     * @param string $type
+     */
+    private function updateLike($work_id, $type) {
+        $Content = M('Content');
+        //类型检查
+        $types = array('add','minus');
+        if(!in_array($type, $types)) {
+            return false;
+        }
+         
+        $data['id'] = $work_id;
+        if($type === 'add') {
+            $data['likes'] = array('exp', '`likes`+1');
+        } else {
+            $data['likes'] = array('exp', '`likes`-1');
+        }
+         
+        return $Content->save($data);
+    }
+    
+    /**
+     * 检查作品是否存在
+     * @param int $work_id
+     */
+    private function checkWorkExists($work_id) {
+        $Content = M('Content');
+        $res = $Content->where(array('id'=>$work_id,'status'=>1))->field('id')->find();
+        if(!$res['id']) {
+            return false;
+        }
+        return true;
+    }
+    
+    /**
+     * 检查是否是自己的作品
+     * @param int $uid
+     * @param int $work_id
+     */
+    private function isMyWork($uid, $work_id) {
+        $Content = M('Content');
+        $res = $Content->where(array('id'=>$work_id, 'uid'=>$uid, 'status'=>1))->field('id')->find();
+        if(!$res['id']) {
+            return false;
+        }
+        return true;
+    }
+    
 }
  
