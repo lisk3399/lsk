8115fa3aabc41cb2bc13e3e4de51ad5b77370831
stevenhu <stevenhu_888@163.com>
2016-08-26 14:23:27 +0800

    消息列表接口

---
+++ Application/Home/Controller/MessageController.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Class MessageController
+ * @name 消息管理
+ */
+
+namespace Home\Controller;
+
+class MessageController extends HomeController {
+
+    //某用户消息列表
+	public function messageList(){
+	    if(IS_POST) {
+	        $uid = is_login();
+	        if(!$uid) {
+	            $this->renderFailed('请先登录', -1);
+	        }
+	        $page = I('page', '1', 'intval');
+	        $rows = I('rows', '10', 'intval');
+	        
+	        //限制单次最大读取数量
+	        if($rows > C('API_MAX_ROWS')) {
+	            $rows = C('API_MAX_ROWS');
+	        }
+	        $map['uid'] = $uid;
+	        $list = M('Message')->where($map)->select();
+	        
+	        if(count($list) == 0) {
+	            $this->renderFailed('没有更多了');
+	        }
+	        
+	        foreach ($list as &$row) {
+	            $row['create_time'] = date('Y-m-d H:i:s', $row['create_time']);
+	        }
+            $this->renderSuccess('消息列表', $list);	        
+	    }
+	}
+}
