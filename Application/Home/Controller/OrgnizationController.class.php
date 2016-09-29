ffd58efecb5985491fc4960c2d5be6e785ef1d46
stevenhu <stevenhu_888@163.com>
2016-09-29 16:15:17 +0800

    排序修改

--- Application/Home/Controller/OrgnizationController.class.php
+++ Application/Home/Controller/OrgnizationController.class.php
@@ -40,15 +40,17 @@ class OrgnizationController extends HomeController {
                 $map['c.status'] = 1;
                 $list = $Content->alias('c')
                 ->field('c.id,c.title,c.description,c.comments,c.likes,c.create_time,t.id as tag_id,t.name')
-                ->join('__TAGS__ t on t.id = c.tag_id')
+                ->join('__TAGS__ t on t.id = c.tag_id', 'left')
                 //->join('__GROUP__ g on c.group_id = g.id')
                 ->where($map)->order('c.is_top desc,c.id desc')->page($page, $rows)->select();
             } else {
-                $list = $Content->alias('c')
+                $subQuery = $Content->alias('c')
                 ->field('c.id,c.title,c.description,c.comments,c.likes,c.create_time,t.id as tag_id,t.name')
-                ->join('__TAGS__ t on t.id = c.tag_id')
+                ->join('__TAGS__ t on t.id = c.tag_id', 'left')
                 //->join('__GROUP__ g on c.group_id = g.id')
-                ->where($map)->group('t.id')->order('t.sort desc,c.is_top desc,c.id desc')->select();
+                ->where($map)->order('t.sort desc,c.is_top desc,c.id desc')->buildSql();
+                $sql = 'select * from '.$subQuery.' t group by t.tag_id';
+                $list = $Content->query($sql);
             }
             
             if(count($list) == 0) {
