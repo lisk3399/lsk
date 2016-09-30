42121b24769f1a131f74cff31ad55bb29023def4
stevenhu <stevenhu_888@163.com>
2016-09-30 16:24:35 +0800

    机构成员数量更新

--- Application/Home/Controller/OrgnizationController.class.php
+++ Application/Home/Controller/OrgnizationController.class.php
@@ -281,7 +281,7 @@ class OrgnizationController extends HomeController {
             }
             
             $org_member_num = M('member_org')->where(array('org_id'=>$org_id))->count();
-            $group_member_num = M('member_group')->where(array('org_id'=>$org_id))->count();
+            $group_member_num = M('member_group')->where(array('group_id'=>array('in', $group_ids)))->count();
             $info['member_num'] = $org_member_num + $group_member_num;
             
             $back_arr = array('/Public/static/app/group_cover_url1.jpg', '/Public/static/app/group_cover_url2.jpg', '/Public/static/app/group_cover_url3.jpg');
